4fe9d66143a886897b5cfe657b1db21f9d9dd9bc
Julia Chupina <yvchupina@sberbank.ru>
2023-08-17 12:34:19 +0300

    add orderId

--- src/components/BurgerConstructor/OrderDetails/index.js
+++ src/components/BurgerConstructor/OrderDetails/index.js
@@ -4,6 +4,7 @@ import orderStyle from "./orderDetails.module.css";
 import cn from 'classnames'
 import postOrder from "../../../utils/postOrder";
 import { ORDER_URL } from '../../../utils/constants'
+import PropTypes from 'prop-types';
 
 function OrderDetails({ ...props }) {
     const [order, setOrder] = useState(null);
@@ -14,7 +15,7 @@ function OrderDetails({ ...props }) {
 
     return (
         <div className={cn(orderStyle.block, "mt-30 ml-20 mr-20 mb-30")}>
-            <div className={cn(orderStyle.number, "mb-8")}><p className="text text_type_digits-large ">{order.order.number}</p></div>
+            <div className={cn(orderStyle.number, "mb-8")}><p className="text text_type_digits-large ">{order?.order?.number}</p></div>
             <p className="text text_type_main-medium ">Идентификатор заказа</p>
             <div className={cn(orderStyle.checkMarkIcon, "mb-10 mt-10")}><CheckMarkIcon type="primary" /></div>
             <p className="text text_type_main-small">Ваш заказ начали готовить</p>
@@ -23,5 +24,10 @@ function OrderDetails({ ...props }) {
     );
 }
 
+OrderDetails.propTypes = {
+    orderId: PropTypes.shape({
+        ingredient: PropTypes.arrayOf(PropTypes.string)
+    })
+}
 
 export default OrderDetails;
