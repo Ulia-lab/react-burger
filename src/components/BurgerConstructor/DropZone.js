da3701e88dea9075cfded09650c53b3c9581e2de
Julia Chupina <yvchupina@sberbank.ru>
2023-08-24 17:20:29 +0300

    add changes

--- src/components/BurgerConstructor/DropZone.js
+++ src/components/BurgerConstructor/DropZone.js
@@ -4,33 +4,33 @@ import { useDrop } from 'react-dnd';
 import { moveItem } from "../../services/actions/constructorItems";
 
 export const DropZone = ({ index, children }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const [{ canDrop, isOver }, dropRef] = useDrop({
-      accept: 'card',
-      drop: (item, monitor) => {
-        const dragIndex = item.index;
-        const hoverIndex = index;
-  
-        if (dragIndex === hoverIndex) {
-          return;
-        }
-  
-        dispatch(moveItem(dragIndex, hoverIndex));
-        item.index = hoverIndex;
-      },
-      collect: (monitor) => ({
-        isOver: monitor.isOver(),
-        canDrop: monitor.canDrop(),
-      }),
-    });
-  
-    const isActive = canDrop && isOver;
+  const [{ canDrop, isOver }, dropRef] = useDrop({
+    accept: 'card',
+    drop: (item, monitor) => {
+      const dragIndex = item.index;
+      const hoverIndex = index;
 
-    return (
-        <div ref={dropRef} className='mb-4' style={{ opacity: isActive ? 0.5 : 1 }}>
-            {children}
-        </div>)
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      dispatch(moveItem(dragIndex, hoverIndex));
+      item.index = hoverIndex;
+    },
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  });
+
+  const isActive = canDrop && isOver;
+
+  return (
+    <div ref={dropRef} className='mb-4' style={{ opacity: isActive ? 0.5 : 1 }}>
+      {children}
+    </div>)
 };
 
 
